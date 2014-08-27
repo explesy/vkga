a3e2aac44933337bedbbc71b47633e3854dab86b
Andrew F <explesy@gmail.com>
2014-08-27 22:14:04 +0400

    Fix some bugs under Windows. Small refactoring of code

--- vkga.py
+++ vkga.py
@@ -6,6 +6,61 @@ import webbrowser
 from string import Template
 import argparse
 
+def get_post_list(group, number_of_posts):
+
+	url = "https://api.vk.com/method/wall.get?domain={}&count={}".format(group, number_of_posts)
+	# We get massive of bytes, so we read it and decode to utf-8 
+	data = urllib.request.urlopen(url).read().decode('utf-8')
+	# Convert data from JSON to Python
+	data_encoded = json.loads(data)
+
+
+	list_of_posts = data_encoded['response'][1:]
+
+	list_of_posts = sorted(list_of_posts, key=lambda k: k['likes']['count'], reverse = True) 
+
+	return list_of_posts
+
+def html_gen(list_of_posts, number_of_posts):
+
+	result = ""
+
+	for i in range(0, number_of_posts):
+		result += "<h1>"
+		result += str(list_of_posts[i]['likes']['count'])
+		result += "</h1>=====</br>"
+		# result += " --> "
+		result += str(list_of_posts[i]['text'])
+		result += "</br>"
+		if 'attachments' in list_of_posts[i]:
+			for j in range(0, len(list_of_posts[i]['attachments'])):
+				if list_of_posts[i]['attachments'][j]['type'] == 'photo':
+					result += "<img src=\""
+					result += str(list_of_posts[i]['attachments'][j]["photo"]['src'])
+					result += "\" />"
+					result += "</br>"
+				elif list_of_posts[i]['attachments'][j]['type'] == 'audio':
+					result += "<a href=\""
+					result += str(list_of_posts[i]['attachments'][j]['audio']['url'])
+					result += "\">"
+					result += str(list_of_posts[i]['attachments'][j]['audio']['artist']) + " - " + str(list_of_posts[i]['attachments'][j]['audio']['title'])
+					result += "</a>"
+					result += "</br>"
+		result += "</br></br>"
+		
+	with open('index.tpl', 'r') as tpl:
+			template_raw = tpl.read()
+
+	page = Template(template_raw)
+	page = page.substitute(insert_data=result) #Exactly page = in other way we get Template object, not a string
+
+	#Important. Without parametr encoding='utf-8' don't working under Windows
+	with open('index.html', 'w', encoding='utf-8') as html:
+			html.write(page)
+
+	webbrowser.open("index.html")
+
+
 
 def all(group, number_of_posts):
 
@@ -46,17 +101,17 @@ def all(group, number_of_posts):
 					result += "</br>"
 		result += "</br></br>"
 		
-
 	with open('index.tpl', 'r') as tpl:
 			template_raw = tpl.read()
 
 	page = Template(template_raw)
 	page = page.substitute(insert_data=result) #Exactly page = in other way we get Template object, not a string
 
-	with open('index.html', 'w') as html:
+	#Important. Without parametr encoding='utf-8' don't working under Windows
+	with open('index.html', 'w', encoding='utf-8') as html:
 			html.write(page)
 
-	webbrowser.open("./index.html")
+	webbrowser.open("index.html")
 
 def main():
 	parser = argparse.ArgumentParser(description='Analyze group in vk and return list of most popular posts ordered by the number of likes')
@@ -64,6 +119,12 @@ def main():
 	parser.add_argument("-c", "--count", help="number of posts from the wall to analyze, integer in range 0..100", type=int, choices=range(101), default=10, metavar='[0..100]')
 	args = parser.parse_args()
 
-	all(args.group, args.count)
+	list = []
+	list = get_post_list(args.group, args.count)
+	html_gen(list, args.count)
+
+
+
+	# all(args.group, args.count)
 
 main()
